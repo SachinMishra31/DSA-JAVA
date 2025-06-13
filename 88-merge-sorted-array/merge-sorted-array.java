139f2669966c4f4d4a281cba06609e4379c72313
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-06-13 18:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (60.35%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;       // Last valid element in nums1
-        int j = n - 1;       // Last element in nums2
-        int k = m + n - 1;   // Last index of nums1
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        // Fill nums1 from the back
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
@@ -13,7 +12,6 @@ class Solution {
             }
         }
 
-        // If nums2 has remaining elements
         while (j >= 0) {
             nums1[k--] = nums2[j--];
         }
