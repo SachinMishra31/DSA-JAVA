e03b74c6cdc8c839df6fbe4e5f68611347c13125
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-06-25 20:39:07 +0530

    Time: 2 ms (96.80%) | Memory: 41.7 MB (81.67%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            // Push opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                // If stack is empty or top doesn't match
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty(); // All brackets matched
+    }
+}
