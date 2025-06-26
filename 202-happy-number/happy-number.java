b3422962b4ddcf3c762d694861a8493bbed16d78
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-06-26 13:46:31 +0530

    Time: 1 ms (67.18%) | Memory: 40.9 MB (40.10%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isHappy(int n) {
         HashSet<Integer> used = new HashSet<>();
-        while(n != 1){
-            if(used.contains(n)){
+        while (n != 1) {
+            if (used.contains(n)) {
                 return false;
             }
             used.add(n);
@@ -12,12 +12,12 @@ class Solution {
         return true;
     }
 
-    public int Square(int num){
+    public int Square(int num) {
         int sum = 0;
-        while(num > 0){
-            int digit = num%10;
-            sum += digit*digit;
-            num = num/10;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num = num / 10;
         }
         return sum;
     }
