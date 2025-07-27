7a4d0a63a96392ede07b648a63984ba246f1f3fe
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-07-27 17:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (22.14%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -5,14 +5,14 @@ class Solution {
 
         if (n > h) return -1;
 
-        // Traverse haystack from 0 to h - n
+        
         for (int i = 0; i <= h - n; i++) {
-            // Check if the substring from i to i+n equals needle
+           
             if (haystack.substring(i, i + n).equals(needle)) {
-                return i; // Found match
+                return i;
             }
         }
 
-        return -1; // No match found
+        return -1;
     }
 }
