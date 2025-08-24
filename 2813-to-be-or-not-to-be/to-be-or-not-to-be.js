1ec91a0b88e2cd59694aef349fb97a802664b3f9
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-08-24 16:43:54 +0530

    Time: 53 ms (6.92%) | Memory: 53.1 MB (75.06%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+
+var expect = function (val) {
+    return {
+        toBe: function (otherval) {
+            if (val === otherval) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function (otherval) {
+            if (val !== otherval) {
+                return true;
+            } else {
+                throw new Error("Equal")
+            }
+        }
+    }
+};
+
