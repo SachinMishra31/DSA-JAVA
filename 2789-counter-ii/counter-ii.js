ed48b18bb70b8c0b46831108518f6e226b188b3e
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-08-25 14:36:14 +0530

    Time: 35 ms (97.50%) | Memory: 55.9 MB (87.68%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,18 @@
+var createCounter = function(init) {
+    let count = init;
+
+    return {
+        increment: function() {
+            count++;
+            return count;
+        },
+        decrement: function() {
+            count--;
+            return count;
+        },
+        reset: function() {
+            count = init;
+            return count;
+        }
+    };
+};
