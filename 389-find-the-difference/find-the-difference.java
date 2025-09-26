60662f09195e2304a9ced09593402356df88f2c1
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2025-09-26 21:33:57 +0530

    Time: 11 ms (16.03%) | Memory: 42 MB (30.61%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (map.containsKey(c) && map.get(c) > 0) {
+                map.put(c, map.get(c) - 1);
+            } else {
+                return c;
+            }
+        }
+
+        return '\0';
+    }
+}
