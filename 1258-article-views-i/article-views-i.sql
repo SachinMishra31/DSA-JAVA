814893a8157824116e370a20cee0645f8738ba8c
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2026-01-05 14:48:43 +0530

    Time: 456 ms (13.95%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,8 @@
-select distinct (author_id ) as id
-from 
-Views
-where author_id = viewer_id
-order by author_id 
+SELECT
+     author_id AS id
+FROM
+    Views
+WHERE 
+    author_id = viewer_id
+GROUP BY author_id
+ORDER BY id ASC
