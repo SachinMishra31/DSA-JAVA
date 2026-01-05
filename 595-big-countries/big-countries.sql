38bc776ef2666176f289fe564eceb659a95035c6
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2026-01-05 14:38:13 +0530

    Time: 270 ms (74.51%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,8 @@
-select name , population , area
-from
-World
-where area >= 3000000 or population >= 25000000
+SELECT
+     name ,
+     population ,
+     area
+FROM
+    World
+WHERE 
+     population >= 25000000 OR area >= 3000000
