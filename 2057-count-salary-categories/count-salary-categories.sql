3ad645163998756e6ff8116918a5130d355d35a9
SACHIN KUMAR MISHRA <sachinkrmishra031@gmail.com>
2026-01-17 09:56:20 +0530

    Time: 1481 ms (64.75%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,14 +1,15 @@
-SELECT category, 
-       COUNT(a.account_id) AS accounts_count
-FROM (
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL
-    SELECT 'High Salary'
-) AS categories
-LEFT JOIN Accounts a
-ON (category = 'Low Salary' AND a.income < 20000)
-   OR (category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000)
-   OR (category = 'High Salary' AND a.income > 50000)
-GROUP BY category;
+SELECT 'Low Salary' AS category,
+       SUM(income < 20000) AS accounts_count
+FROM Accounts
+
+UNION
+
+SELECT 'Average Salary' AS category,
+       SUM(income BETWEEN 20000 AND 50000) AS accounts_count
+FROM Accounts
+
+UNION
+
+SELECT 'High Salary' AS category,
+       SUM(income > 50000) AS accounts_count
+FROM Accounts;
